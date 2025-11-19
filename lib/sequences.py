73ecdf878759488cf370ed8d97e5ffb61cfa456e
Ndet0 <festusndet00@gmail.com>
2025-11-19 15:08:06 +0300

    initial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    """Print the first `length` Fibonacci numbers as a Python list.
+
+    Examples:
+        length = 0 -> prints []\n
+        length = 1 -> prints [0]\n
+        length = 2 -> prints [0, 1]\n
+    """
+    if length <= 0:
+        print([])
+        return
+
+    # build fibonacci list
+    fib = [0]
+    if length == 1:
+        print(fib)
+        return
+
+    fib.append(1)
+    while len(fib) < length:
+        fib.append(fib[-1] + fib[-2])
+
+    print(fib)
